4c4f895cbc1f3e3cb524e2b945c392b1683687f2
pale-ale <alex.baucke@tutanota.com>
2022-09-11 12:40:46 +0200

    fix crash on tileset file not found

--- MapParser/Tileset.hpp
+++ MapParser/Tileset.hpp
@@ -11,39 +11,48 @@ namespace tmx {
  * @brief Contains the tiles, dimensions, source image etc. needed to display a tile.
  */
 struct Tileset : public DataNode {
-  Tileset(tinyxml2::XMLElement *element, DataNode *parent, std::filesystem::path mapPath) : DataNode(element, parent), mapPath{mapPath}{}
+  Tileset(tinyxml2::XMLElement *element, DataNode *parent, std::filesystem::path mapPath)
+      : DataNode(element, parent), mapPath{mapPath} {}
   virtual void commit_data() override {}
   virtual void update_data() override {
+    // Obtain the relative tileset file path and it's first global tile id.
     bool errors = false;
     errors |= (bool)element->QueryAttribute("firstgid", &firstgid);
     errors |= (bool)element->QueryAttribute("source", &relativeTilesetPath);
     if (errors) {
-      printf("Errors in tileset node in the map file.\n");
+      printf("[Tileset]: Errors in tileset node of map file.\n");
     }
 
+    // Convert the relative path to an absolute one, abort if it doesn't exist.
     auto mapFolder = mapPath.remove_filename();
-    tilesetPath = canonical(mapFolder.append(std::string(relativeTilesetPath)));
+    tilesetPath = std::filesystem::weakly_canonical(
+        mapFolder.append(std::string(relativeTilesetPath)));
     if (!std::filesystem::exists(tilesetPath)) {
-      printf("Tileset path '%s' does not exist.\n", tilesetPath.c_str());
+      printf("[Tileset]: Error reading tileset at '%s': File does not exist.\n",
+             tilesetPath.c_str());
+      return;
     }
 
-    // Open the tsx that is referenced and read the tileset data.
+    // Open the tsx and read the tileset data.
     tinyxml2::XMLDocument xmlDoc;
     xmlDoc.LoadFile(tilesetPath.c_str());
     auto tilesetElement = xmlDoc.FirstChildElement();
-    errors |= (bool)tilesetElement->QueryAttribute("tilewidth", &tilewidth);
-    errors |= (bool)tilesetElement->QueryAttribute("tileheight", &tileheight);
-    errors |= (bool)tilesetElement->QueryAttribute("tilecount", &tilecount);
-    errors |= (bool)tilesetElement->QueryAttribute("columns", &columns);
-    const char *cname;
-    errors |= (bool)tilesetElement->QueryAttribute("name", &cname);
-    name = cname;
-    auto imageNode = tilesetElement->FirstChildElement();
-    errors |= (bool)imageNode->QueryAttribute("source", &relativeImagePath);
-    auto tilesetFolder = std::filesystem::path(tilesetPath).remove_filename();
-    imagePath = std::filesystem::canonical(tilesetFolder.append(relativeImagePath));
+    errors |= !tilesetElement;
+    if (tilesetElement) {
+      errors |= (bool)tilesetElement->QueryAttribute("tilewidth", &tilewidth);
+      errors |= (bool)tilesetElement->QueryAttribute("tileheight", &tileheight);
+      errors |= (bool)tilesetElement->QueryAttribute("tilecount", &tilecount);
+      errors |= (bool)tilesetElement->QueryAttribute("columns", &columns);
+      const char *cname;
+      errors |= (bool)tilesetElement->QueryAttribute("name", &cname);
+      name = cname;
+      auto imageNode = tilesetElement->FirstChildElement();
+      errors |= (bool)imageNode->QueryAttribute("source", &relativeImagePath);
+      auto tilesetFolder = std::filesystem::path(tilesetPath).remove_filename();
+      imagePath = std::filesystem::canonical(tilesetFolder.append(relativeImagePath));
+    }
     if (errors) {
-      printf("Errors in tileset file at '%s'.\n", imagePath.c_str());
+      printf("[Tileset]: Errors in tileset file at '%s'.\n", imagePath.c_str());
     }
   }
 
